47f228396f7ff9f4871dde1504e74d3f4b80e391
Soledad Galli <solegalli@protonmail.com>
2022-08-12 18:45:34 +0200

    commented target blank in theme js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -35,7 +35,7 @@
     
     })(jQuery);
 
-// Blank Target External Links
-$(document.links).filter(function() {
-return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
+//// Blank Target External Links
+//$(document.links).filter(function() {
+//return this.hostname != window.location.hostname;
+//}).attr('target', '_blank');
